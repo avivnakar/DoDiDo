8845feeaed9f86dfbe1a0a7f45854776b2ee68da
Gal Rondel <gal31010@gmail.com>
2020-05-21 01:52:29 +0300

    card prev v1.4

--- frontend/src/cmps/board/CardPreiview.jsx
+++ frontend/src/cmps/board/CardPreiview.jsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 export function CardPreiview(props) {
     console.log('this card:', props);
     const { card } = props
+    
     return (
         <div>
             <span>{card.title}</span>
             <div>
-                {card.cheklists && <span>📝</span>}
-                {card.cardMembers && <span>👤</span>}
-                {card.attachments && <span>📦</span>}
+                {card.cheklists && <span>{card.cheklists.length}📝</span>}
+                {card.cardMembers && <span>{card.cardMembers.length}👤</span>}
+                {card.attachments && <span>{card.attachments.length}📦</span>}
                 {card.dueDate && <span>⏰</span>}
 
             </div>
