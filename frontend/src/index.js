afafd14a1d34b90a7497f320ea6cbd007b7a6799
avivnakar <avivnakar@gmail.com>
2020-05-20 17:04:14 +0300

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import './styles/global.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+ import {store} from './store/store'
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//register before build
+serviceWorker.unregister();
