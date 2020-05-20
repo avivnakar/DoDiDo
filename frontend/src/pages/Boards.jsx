da339eb559e8388e8dee7b912b2a33af619d47b5
Gal Rondel <gal31010@gmail.com>
2020-05-20 20:52:56 +0300

    board

--- frontend/src/pages/Boards.jsx
+++ frontend/src/pages/Boards.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-// import { BoardsList } from './cmps/BoardsList.jsx';
+import { BoardsList } from './cmps/BoardsList.jsx';
 
 
 export class Boards extends Component {
     render() {
         return (
             <div className="list-container">
-               סס
+               <Route component={BoardsList} path="/" />
             </div>
         )
     }
