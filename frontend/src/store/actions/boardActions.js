39752c9072b650618e1d07472c0f89b202da8650
avivnakar <avivnakar@gmail.com>
2020-05-20 23:40:09 +0300

    loading boards reducer works great, push to aviv branch and master branch

--- frontend/src/store/actions/boardActions.js
+++ frontend/src/store/actions/boardActions.js
@@ -1,6 +1,12 @@
 import { boardService } from '../../services/boardService';
 
-// export async function addCard() {
-//     const user = await boardService
-//     return dispatch => dispatch({ type: 'LOGIN', user });
+// export async function loadBoards(filter) {
+//     const boards = await boardService.query(filter)
+//     return dispatch => dispatch({ type: 'SET_BOARDS', boards });
 // }
+export function loadBoards(criteria) {
+        return dispatch => {
+        return boardService.query(criteria)
+            .then(boards => dispatch({ type: 'SET_BOARDS', boards }))
+    }
+}
