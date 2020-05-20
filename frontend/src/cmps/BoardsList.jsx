739c42f9e1c71a7937078b0f1647f6a272134c61
Gal Rondel <gal31010@gmail.com>
2020-05-20 21:00:00 +0300

    boardcmp

---
+++ frontend/src/cmps/BoardsList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export  class BoardsList extends Component {
+    render() {
+        return (
+            <div>
+                xxxxxx
+            </div>
+        )
+    }
+}
--- frontend/src/pages/Boards.jsx
+++ frontend/src/pages/Boards.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import { BoardsList } from './cmps/BoardsList.jsx';
+import { BoardsList } from '../cmps/BoardsList.jsx';
 
 
 export class Boards extends Component {
     render() {
         return (
             <div className="list-container">
-               <Route component={BoardsList} path="/" />
+               <BoardsList />
             </div>
         )
     }
