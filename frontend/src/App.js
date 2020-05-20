7359e000943a69bdfa9f51250170b77396fc6f65
avivnakar <avivnakar@gmail.com>
2020-05-20 19:54:57 +0300

    hompage fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import { BrowserRouter, Route } from 'react-router-dom';
-import HomePage from './pages/Homepage.jsx';
+import {Homepage} from './pages/Homepage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <header>sccsc</header>
-      <Route component={HomePage} path="/" />
+      <Route component={Homepage} path="/" />
     </BrowserRouter>
   );
 }
