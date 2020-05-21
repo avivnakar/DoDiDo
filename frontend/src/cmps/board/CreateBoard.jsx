8ed7039c345bb842d697aaa7833b784d1d8c3805
yuvaal12 <yuvaal9@gmail.com>
2020-05-21 16:09:56 +0300

    yuval dont create board elem

--- frontend/src/cmps/board/CreateBoard.jsx
+++ frontend/src/cmps/board/CreateBoard.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import image from '../../assets/imgs/1.png'
+import image from '../../assets/imgs/3.jpg'
 export function CreateBoard() {
     return (
         <section>
-            <input type="text" placeholder="Add board title" />
+            <div className="min-board">
+                <img src={image} alt="" />
+                <input type="text" placeholder="Add board title" />
+            </div>
+
             <button>Add background</button>
             <div className="bg-container">
                 <h2>board background</h2>
@@ -11,21 +15,47 @@ export function CreateBoard() {
                     <p>photos</p>
                     <a href="">see more</a>
                 </div>
-                <div className="photos flex space-even">
+                <div className="photos column flex space-even">
                     {/* render photos here */}
-                    <img src={image} alt="" />
-
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
                 </div>
                 <div className="colors-title flex space-between">
                     <p>colors</p>
                     <a href="">see more</a>
                 </div>
-                <div className="colors flex space-even">
+                <div className="colors column flex space-even">
                     {/* render colors here */}
-                    {/* <img src={require('../assets/imgs/2.jpg')} alt="" />
-                    <img src={require('../assets/imgs/1.png')} alt="" />
-                    <img src={require('../assets/imgs/2.jpg')} alt="" /> */}
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
+                    <div className="img-line">
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                        <img src={image} alt="" />
+                    </div>
                 </div>
+                <button className="create-board">Create Board</button>
             </div>
         </section>
     )
