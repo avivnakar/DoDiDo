8e1188b6a5d8adc5dbd1759811cafee45c012bdb
Gal Rondel <gal31010@gmail.com>
2020-05-21 11:01:07 +0300

    mergeinconflit fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import { Homepage } from './pages/Homepage.jsx';
-<<<<<<< HEAD
-=======
 import { Boards } from './pages/Boards.jsx';
 import { BoardDetails } from './pages/BoardDetails.jsx';
->>>>>>> 60ea8b5593b0c662523ede98c2618fe3625054d7
+
 
 function App() {
   return (
     <BrowserRouter>
-
       <Switch>
-<<<<<<< HEAD
-=======
         <Route component={Boards} path="/:username/boards" />
         <Route component={BoardDetails} path="/b/:boardid/:boardname"/>
->>>>>>> 60ea8b5593b0c662523ede98c2618fe3625054d7
         <Route component={Homepage} path="/" />
       </Switch>
     </BrowserRouter>
