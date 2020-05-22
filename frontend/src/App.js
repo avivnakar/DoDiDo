6ff8031f07a685913727cc2b179eec097f6e409c
avivnakar <avivnakar@gmail.com>
2020-05-22 10:29:40 +0300

    added layout borders and comments variables.scss, moved massive comment chunk in BoardDetails to bottom, added new route CardDetails. pulling master branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route,  Switch } from 'react-router-dom';
 import { Homepage } from './pages/Homepage.jsx';
 import { Boards } from './pages/Boards.jsx';
 import { BoardDetails } from './pages/BoardDetails.jsx';
-
+import { CardDetails } from './cmps/board/card/CardDetails.jsx';
 
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
   return (
 
     <BrowserRouter>
+        <Route component={CardDetails} path="/c/:cardId/:cardTitle" />
       <Switch>
         <Route component={Boards} path="/:username/boards" />
         <Route component={BoardDetails} path="/b/:boardId/:boardname" />
