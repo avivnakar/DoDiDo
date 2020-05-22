aabe0a3fe5dd7b05dee3142a202e12af8cffc42d
avivnakar <avivnakar@gmail.com>
2020-05-22 19:42:38 +0300

    added NavLink to home

--- frontend/src/cmps/board/BoardNav.jsx
+++ frontend/src/cmps/board/BoardNav.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import image from '../../assets/imgs/Logo.png'
 import { FaTh, FaColumns, FaPlus, FaWeightHanging, FaUserCircle, FaInfo, FaHome } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 export function BoardNav(props) {
 
     return (
         <nav className="board-nav flex space-between align-center justify-center">
             <ul className="main-links left-links flex space-even">
                 <li><FaTh /></li>
-                <li><FaHome /></li>
+                <li><NavLink exact to="/"><FaHome/></NavLink></li>
                 <li><FaColumns /></li>
                 <input type="text" name="search" placeholder="Search.." />
             </ul>
