ec02e7f2bb47f6776d5ec9cd6adb8974c940838d
Gal Rondel <gal31010@gmail.com>
2020-05-24 15:49:02 +0300

    labels v1.01

--- frontend/src/cmps/card/AddLabels.jsx
+++ frontend/src/cmps/card/AddLabels.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { LabelList } from '../board/LabelList.jsx';
+
 export class AddLabels extends Component {
     render() {
+        const { board, updateboard, cardLabels } = this.props
         return (
             <div>
-                
+                Labels
+                <LabelList labels={board.labels} /><button>+Add Label</button>
             </div>
         )
     }
