fd7726ef9c35f474e2291b72119f35a52fbb4f09
avivnakar <avivnakar@gmail.com>
2020-05-27 10:03:49 +0300

    show password feature

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,11 +3,19 @@ import { connect } from 'react-redux';
 import { logout, login, signup } from '../store/actions/userActions';
 
 class _Signup extends Component {
-
+    state = {
+        isShown: false
+    }
     componentDidUpdate() {
         !this.props.user && console.log(this.props.user)
     }
+    toggleShow = (ev) => {
+        const isShown = ev.target.checked
+        this.setState({ isShown });
+    }
     render() {
+        const { toggleShow } = this
+        const { isShown } = this.state;
         return <section>
             <form method="post">
                 <ul>
@@ -16,12 +24,13 @@ class _Signup extends Component {
                         <input type="text" name="fullName" placeholder="Full Name" required />
                     </fieldset></li>
                     <li><fieldset>
-                        <legend>Enter Your Full Name:</legend>
-                        <input type="password" name="username" placeholder="Username" required />
+                        <legend>Username:</legend>
+                        <input type="text" name="username" placeholder="Username" required />
                     </fieldset></li>
                     <li><fieldset>
                         <legend>Password:</legend>
-                        <input type="password" name="password" placeholder="Password" required />
+                        <input type="checkbox" onChange={toggleShow} checked={isShown} name="isShown" id="" />
+                        {<input type={isShown ? 'text' : 'password'} name="password" placeholder="Password" required />}
                     </fieldset></li>
                     <li><fieldset>
                         <legend>Phone number:</legend>
