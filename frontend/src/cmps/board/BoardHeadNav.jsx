661780d59ae43b7b15d40f641ddfc375c69a1456
Gal Rondel <gal31010@gmail.com>
2020-05-27 03:10:23 +0300

    comments + checklists

--- frontend/src/cmps/board/BoardHeadNav.jsx
+++ frontend/src/cmps/board/BoardHeadNav.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { FaTrashAlt } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { BoardTitle } from './BoardTitle.jsx'
-
+import { MiniUser } from '../MiniUser';
 export function BoardHeadNav(props) {
-
+    function notFunc(){
+        return 
+    }
     return (
         <header className="board-header flex space-between justify-center align-center">
             <BoardTitle board={props.board} updateBoard={props.updateBoard} />
+            {/* <MiniUser users={props.board.members} command={notFunc} /> */}
             <div className="board-menu  flex">
                 <Link to="#" className="board-sec-nav-icons trash"><FaTrashAlt /></Link>
                 <Link to="#" className="board-sec-nav-icons">Add Members</Link>
