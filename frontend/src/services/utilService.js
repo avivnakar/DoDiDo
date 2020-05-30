3c4ee1c98f06e3f0a64db6935c8fcc7f9c8e026c
Gal Rondel <gal31010@gmail.com>
2020-05-30 20:19:34 +0300

    merge

--- frontend/src/services/utilService.js
+++ frontend/src/services/utilService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 function makeId(length=3) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -21,6 +23,19 @@ function getIdxById(id,arr)
 }
 export const utilService= {
     makeId,
-    getIdxById
+    getIdxById,
+    uploadImg
 }
 
+function uploadImg(ev) {
+    const CLOUD_NAME = 'rondeliahu'; // find it in your cloudinary account (main page)
+    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
+
+    const formData = new FormData();
+    formData.append('file', ev.target.files[0]);
+    formData.append('upload_preset', 'DoDiDo'); // second parameter is the upload preset (you can find it in cloudinary settings)
+
+    return axios.post(UPLOAD_URL, formData)
+        .then(res => res.data.url)
+        .catch(err => console.error(err))
+}
